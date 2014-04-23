3f7b9878e7a99e80a8ecfdc5a221d548f7773c1a
Mike <mike@example.com>
2014-04-23 01:31:46 +0000

    adds a line to the routes file

--- git_practice/config/routes.rb
+++ git_practice/config/routes.rb
@@ -1,4 +1,6 @@
 GitPractice::Application.routes.draw do
+  # Made some changes 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
